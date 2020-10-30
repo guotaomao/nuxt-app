6960f4ea3f1b2e1883083fc0134e99572e475362
maoguotao <v_guotao.mao@fowo.com>
2020-10-30 13:42:39 +0800

    :rocket:

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>content</div>
+    <div>
+      <p>{{content}}</p> 
+      <button @click="refresh">refresh</button>
+    </div>
 </template>
 
 <script>
 export default {
     layout: 'blog',
-
+    asyncData () {
+        console.log('process', process.client ? 'client' : 'server')
+        return { content: 'created at' + new Date() }
+    },
+    methods: {
+        refresh () {
+            this.$nuxt.refresh()
+        }
+    }
 }
 </script>
 
--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -4,16 +4,16 @@
 
 <script>
   export default {
-     async asyncData (context) {
-         const id = context.params.id
-         try {
-             // using the nuxtjs/http module here exposed via context.app
-             const post = await context.app.$http.$get(`https://api.nuxtjs.dev/posts/${id}`)
-             return { post }
-         } catch (error) {
-             context.error(error) // show the nuxt error page with the thrown error
-         }
-     }  
+    //  async asyncData (context) {
+    //      const id = context.params.id
+    //      try {
+    //          // using the nuxtjs/http module here exposed via context.app
+    //          const post = await context.app.$http.$get(`https://api.nuxtjs.dev/posts/${id}`)
+    //          return { post }
+    //      } catch (error) {
+    //          context.error(error) // show the nuxt error page with the thrown error
+    //      }
+    //  }  
   } 
 </script>
 
